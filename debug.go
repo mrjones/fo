0cf07ebc96aec8d90200f37cb0dd367f4a8dcae8
Matt Jones <jonesmr+github@gmail.com>
2012-05-26 17:52:46 +0000

    moved stuff to debug

--- debug.go
+++ debug.go
@@ -12,3 +12,26 @@ func print(scores map[TeamID]map[StatID]int, stats map[TeamID]StatLine) {
 		}
 	}
 }
+
+func FormatBattingStats(stats StatLine) string {
+	return fmt.Sprintf("AVG:%0.3f HR:%02d R:%03d RBI:%03d SB:%02d",
+		stats[B_BATTING_AVG],
+		int(stats[B_HOME_RUNS]),
+		int(stats[B_RUNS]),
+		int(stats[B_RUNS_BATTED_IN]),
+		int(stats[B_STOLEN_BASES]))
+}
+
+func FormatPitchingStats(stats StatLine) string {
+	whip, ok := stats[P_WHIP]
+	if !ok {
+		whip = (stats[P_WALKS] + stats[P_HITS]) / stats[P_INNINGS]
+	}
+
+	return fmt.Sprintf("W:%02d S:%02d K:%03d ERA:%0.2f WHIP:%0.2f",
+		int(stats[P_WINS]),
+		0, // TODO: saves
+		int(stats[P_STRIKE_OUTS]),
+		stats[P_EARNED_RUN_AVERAGE],
+		whip)
+}
--- fo.go
+++ fo.go
@@ -24,29 +24,6 @@ func VerboseGetStat(player PlayerID, statname StatID, client StatsClient) {
 	log.Printf("%s, %v -> %f", player, statname, statval)
 }
 
-func FormatBattingStats(stats StatLine) string {
-	return fmt.Sprintf("AVG:%0.3f HR:%02d R:%03d RBI:%03d SB:%02d",
-		stats[B_BATTING_AVG],
-		int(stats[B_HOME_RUNS]),
-		int(stats[B_RUNS]),
-		int(stats[B_RUNS_BATTED_IN]),
-		int(stats[B_STOLEN_BASES]))
-}
-
-func FormatPitchingStats(stats StatLine) string {
-	whip, ok := stats[P_WHIP]
-	if !ok {
-		whip = (stats[P_WALKS] + stats[P_HITS]) / stats[P_INNINGS]
-	}
-
-	return fmt.Sprintf("W:%02d S:%02d K:%03d ERA:%0.2f WHIP:%0.2f",
-		int(stats[P_WINS]),
-		0, // TODO: saves
-		int(stats[P_STRIKE_OUTS]),
-		stats[P_EARNED_RUN_AVERAGE],
-		whip)
-}
-
 func (fo *FO) Optimize() {
 	//	fo.zipsProjectMyRoster()
 	//	fo.myCurrentStats()
@@ -86,22 +63,12 @@ func (fo *FO) Optimize() {
 
 	fmt.Printf("Projections\n")
 	scores := score(teamProjections)
-	a := 0
-	for t := range(scores) {
-		fmt.Printf("TEAM %d: %d\n", t, scores[t])
-		a += scores[t]
-	}
-	fmt.Println(a)
-	
+	print(scores)
 
 	fmt.Printf("\nActuals\n")
 	scores = score(*teamStats)
-	a = 0
-	for t := range(scores) {
-		fmt.Printf("TEAM %d: %d\n", t, scores[t])
-		a += scores[t]
-	}
-	fmt.Println(a)
+	print(scores)
+
 
 	// Full Docs:
 	// http://developer.yahoo.com/fantasysports/guide/index.html
