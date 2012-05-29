fceb3f6721075e3d146717a78282aa688e262ae5
Matt Jones <jonesmr@gmail.com>
2012-05-29 09:42:12 -0400

    deleted EnsureCache

--- caching.go
+++ caching.go
@@ -3,7 +3,6 @@ package main
 import (
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -122,31 +121,6 @@ func (c MemKVStore) Age(k string) *time.Duration {
 }
 
 
-func EnsureCache(url, filename string) (string, error) {
-	age := fileAge(filename)
-
-	if age == nil {
-		log.Printf("Can't find %s. Downloading", filename)
-		httpFetchToFile(url, filename)
-	} else {
-		ageHours := *age / time.Hour
-		if ageHours > 24*30 {
-			log.Printf("%s is too old (%d h). Downloading.", filename, ageHours)
-			httpFetchToFile(url, filename)
-		} else {
-			log.Printf("%s is fresh enough (%d h). Not downloading.", filename, ageHours)
-		}
-	}
-
-	bits, err := ioutil.ReadFile(filename)
-
-	if err != nil {
-		return "", err
-	}
-
-	return string(bits), nil
-}
-
 func fileAge(filename string) *time.Duration {
  	file, err := os.Open(filename)
 	if err != nil {
@@ -162,16 +136,6 @@ func fileAge(filename string) *time.Duration {
 	age := time.Since(fileinfo.ModTime())
 
 	return &age
-}
-
-func httpFetchToFile(url, filename string) error {
-	log.Printf("Fetching %s to %s", url, filename)
-	body, err := httpGetBody(url)
-	if err != nil {
-		return err
-	}
-
-	return ioutil.WriteFile(filename, []byte(body), 0644)
 }
 
 func httpGetBody(url string) (string, error) {
